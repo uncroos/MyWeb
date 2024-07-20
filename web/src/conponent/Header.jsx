5cffd760b646a281695e990ee3ee57a7534c53ab
UNCROOS <any492266@gmail.com>
2024-07-21 00:52:39 +0900

    init

---
+++ web/src/conponent/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+    return (
+        <header>
+            <div className="main-title">
+                <h1>안요한</h1>
+                <p>서울로봇고등학교 첨단로봇시스템과</p>
+                <p>머신러닝 개발자</p>
+            </div>
+        </header>
+    );
+}
