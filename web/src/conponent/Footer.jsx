db35040d76e10ff204680cc6aa28c7c38812fe0f
UNCROOS <any492266@gmail.com>
2024-07-21 10:47:03 +0900

    ㅑㅜㅑㅅ

--- web/src/conponent/Footer.jsx
+++ web/src/conponent/Footer.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Footer.css';
+import { FaInstagram, FaGithub, FaLinkedin, FaBlog } from 'react-icons/fa';
 
 export default function Footer() {
     return (
         <footer>
             <p>© 2024 안요한. 모든 권리를 보유.</p>
+            <a href="https://www.instagram.com/uncroos" target="_blank" rel="noopener noreferrer">
+                <FaInstagram /> 
+            </a>
+            <a href="https://github.com/uncroos" target="_blank" rel="noopener noreferrer">
+                <FaGithub /> 
+            </a>
+            <a href="https://www.linkedin.com/in/%EC%9A%94%ED%95%9C-%EC%95%88-b64a53315/?trk=opento_sprofile_topcard" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin /> 
+            </a>
+            <a href="https://blog.naver.com/uncroos" target="_blank" rel="noopener noreferrer">
+                <FaBlog /> 
+            </a>
         </footer>
     );
 }
